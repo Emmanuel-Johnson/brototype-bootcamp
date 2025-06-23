5fe3cfa7b882da40ade044a3079033c69240a1d9
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-06-23 23:04:35 +0530

    Edit Pickling, Quitting Every Bad Habits

---
+++ 1_Python/Practical/research.py
@@ -0,0 +1,10 @@
+import os
+
+with open("my_file.txt", "w") as f:
+    f.write("New file Created Successfully, and this is the file.")
+
+if os.path.exists("my_file.txt"):
+    os.remove("my_file.txt")
+    print("File Removed Successfully")
+else:
+    print("File not exists")
