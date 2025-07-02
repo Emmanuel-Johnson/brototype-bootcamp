f3a003158c742390beb12270491eb146d8649f46
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-07-02 23:57:04 +0530

    Add random number, decorator, variadic func, throwing error, error handling

--- 1_Python/Practical/04_decorator_to_lowercase_return.py
+++ 1_Python/Practical/04_decorator_to_lowercase_return.py
@@ -14,3 +14,23 @@ def greet():
     return "HELLO WORLD"
 
 print(greet()) 
+
+
+""" 
+def to_lowercase_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if isinstance(result, str):
+            return result.lower()
+        return result
+    return wrapper
+
+
+
+def greet(message):
+    return message
+
+greet = to_lowercase_decorator(greet)
+
+print(greet("HELLO WORLD"))  
+"""
--- 1_Python/Practical/06_throwing_error.py
+++ 1_Python/Practical/06_throwing_error.py
@@ -1,12 +1,5 @@
 # throwing error
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero!") 
-    return a / b
-
-try:
-    result = divide(10, 0)
-    print(result)
-except ValueError as e:
-    print(f"Error caught: {e}")
+x = -1
+if x < 0:
+    raise ValueError("x must be non-negative")
