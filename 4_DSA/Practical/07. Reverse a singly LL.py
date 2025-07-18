60fe6f54e6bb881437c15060f6e9baa900113c32
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-07-18 23:18:23 +0530

    Singly Linked list add at begin, add at end, array to sll, display, delete, insert after and before, print reverse

--- 4_DSA/Practical/07. Reverse a singly LL.py
+++ 4_DSA/Practical/07. Reverse a singly LL.py
@@ -2,171 +2,106 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
-class SinglyLinkedList:
+        
+class SLL:
     def __init__(self):
         self.head = None
-
-def array_to_linked_list(arr):
-    sll = SinglyLinkedList()
-    for item in arr:
-        sll.append(item)
-    return sll
-
-def append(self, data):
-    new_node = Node(data)
-    if not self.head:
-        self.head = new_node
-        return
-    current = self.head
-    while current.next:
-        current = current.next
-    current.next = new_node
-
-SinglyLinkedList.append = append
-
-def prepend(self, data):
-    new_node = Node(data)
-    new_node.next = self.head
-    self.head = new_node
-
-SinglyLinkedList.prepend = prepend
-
-sll = array_to_linked_list([1, 2, 3])
-sll.append(4)    # Add at end
-sll.prepend(0)   # Add at beginning
-
-def delete_value(self, value):
-    if not self.head:
-        return
-
-    if self.head.data == value:
-        self.head = self.head.next
-        return
-
-    current = self.head
-    while current.next and current.next.data != value:
-        current = current.next
-
-    if current.next:
-        current.next = current.next.next
-
-SinglyLinkedList.delete_value = delete_value
-
-def insert_after(self, x, data):
-    current = self.head
-    while current and current.data != x:
-        current = current.next
-    if current:
-        new_node = Node(data)
-        new_node.next = current.next
-        current.next = new_node
-
-def insert_before(self, x, data):
-    if not self.head:
-        return
-    if self.head.data == x:
-        self.prepend(data)
-        return
-    current = self.head
-    while current.next and current.next.data != x:
-        current = current.next
-    if current.next:
-        new_node = Node(data)
-        new_node.next = current.next
-        current.next = new_node
-
-SinglyLinkedList.insert_after = insert_after
-SinglyLinkedList.insert_before = insert_before
-
-def print_list(self):
-    current = self.head
-    while current:
-        print(current.data, end=" -> ")
-        current = current.next
-    print("None")
-
-def print_reverse(self):
-    def _print_reverse(node):
-        if node:
+    
+    def add_at_beginning(self, data):
+        newnode = Node(data)
+        newnode.next = self.head
+        self.head = newnode
+        
+    def add_at_end(self, data):
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = newnode
+    
+    def array_to_sll(self, arr):
+        for i in arr:
+            self.add_at_end(i)
+        
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end = ' --> ')
+            current = current.next
+        print("None")
+    
+    def delete(self, data):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+        print(f"{data} not found in the linked list")
+    
+    def insert_node_before_data(self, before, data):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        if self.head.data == before:
+            self.add_at_beginning(data)
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == before:
+                newnode = Node(data)
+                newnode.next = current.next
+                current.next = newnode
+                return
+            current = current.next
+        print(f"{before} not found in the linked list")
+        
+    def insert_node_after_data(self, after, data):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        current = self.head
+        while current:
+            if current.data == after:
+                newnode = Node(data)
+                newnode.next = current.next
+                current.next = newnode
+                return
+            current = current.next
+        print(f"{data} not found in the linked list")
+    
+    def print_reverse(self):
+        def _print_reverse(node):
+            if not node:
+                return
             _print_reverse(node.next)
-            print(node.data, end=" <- ")
-    _print_reverse(self.head)
-    print("None")
-
-SinglyLinkedList.print_list = print_list
-SinglyLinkedList.print_reverse = print_reverse
-
-def remove_duplicates(self):
-    current = self.head
-    while current and current.next:
-        if current.data == current.next.data:
-            current.next = current.next.next
-        else:
+            print(node.data, end = ' <-- ')
+        _print_reverse(self.head)
+        print("None")
+        
+    def print_reverse_iteration(self):
+        stack = []
+        current = self.head
+        while current:
+            stack.append(current.data)
             current = current.next
+        while stack:
+            print(f"{stack.pop()}", end = " <-- ")
+        print("None")
+        
+        
+        
+sll = SLL()
 
-SinglyLinkedList.remove_duplicates = remove_duplicates
-
-sll = array_to_linked_list([1, 1, 2, 3, 3, 4])
-sll.remove_duplicates()
-sll.print_list()
-
-def remove_unsorted_duplicates(self):
-    if not self.head:
-        return
-
-    seen = set()
-    current = self.head
-    seen.add(current.data)
-
-    while current.next:
-        if current.next.data in seen:
-            current.next = current.next.next
-        else:
-            seen.add(current.next.data)
-            current = current.next
-
-SinglyLinkedList.remove_unsorted_duplicates = remove_unsorted_duplicates
-
-sll = array_to_linked_list([3, 1, 4, 1, 2, 3, 4])
-print("Before removing duplicates:")
-sll.print_list()
-
-sll.remove_unsorted_duplicates()
-print("After removing duplicates:")
-sll.print_list()
-
-def reverse(self):
-    prev = None
-    current = self.head
-
-    while current:
-        next_node = current.next  # store next node
-        current.next = prev       # reverse the link
-        prev = current            # move prev forward
-        current = next_node       # move current forward
-
-    self.head = prev  # update head to the last node
-
-SinglyLinkedList.reverse = reverse
-
-sll = array_to_linked_list([1, 2, 3, 4, 5])
-print("Original List:")
-sll.print_list()
-
-sll.reverse()
-print("Reversed List:")
-sll.print_list()
-
-def reverse_recursive(self):
-    def _reverse_recursive(current, prev):
-        if not current:
-            return prev
-        next_node = current.next
-        current.next = prev
-        return _reverse_recursive(next_node, current)
-
-    self.head = _reverse_recursive(self.head, None)
-
-SinglyLinkedList.reverse_recursive = reverse_recursive
-
+arr = [1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 3, 3, 3, 2, 1]
+sll.array_to_sll(arr)
+sll.display()
