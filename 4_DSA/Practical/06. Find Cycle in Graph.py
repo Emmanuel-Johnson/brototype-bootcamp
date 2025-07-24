fab26a7822763fed9d72736e51f9a67841e04cf2
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-07-24 20:50:38 +0530

    Graph Insertion Adjacency Matrix

---
+++ 4_DSA/Practical/06. Find Cycle in Graph.py
@@ -0,0 +1,38 @@
+# Python Program To Implement Graph Insertion Operation | Add Node | Adjacency Matrix | Data Structure
+
+def add_node(v):
+    global node_count 
+    if v in nodes:
+        print("V is already present in the graph")
+    else:
+        node_count += 1
+        nodes.append(v)
+        for n in graph:
+            n.append(0)
+        temp = []
+        for i in range(node_count):
+            temp.append(0)
+        graph.append(temp)
+        
+def print_graph():
+    for i in range(node_count):
+        for j in range(node_count):
+            print(graph[i][j], end = " ")
+        print()
+        
+nodes = []        
+graph = []
+node_count = 0
+
+print("Before Adding Nodes")
+print(nodes)
+print(graph)
+print("After Adding Nodes")
+add_node("A")
+add_node("B")
+add_node("C")
+print(nodes)
+print(graph)
+
+print_graph()
+
