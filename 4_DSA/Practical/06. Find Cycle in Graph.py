b342af735a02161f1e1920850a600a0190894c86
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2025-07-26 22:24:55 +0530

    Edit Graph Added more things in that, DFS Recursive

--- 4_DSA/Practical/06. Find Cycle in Graph.py
+++ 4_DSA/Practical/06. Find Cycle in Graph.py
@@ -1,4 +1,4 @@
-# Python Program To Implement Graph Insertion Operation | Add Node | Adjacency Matrix | Data Structure
+"""# Python Program To Implement Graph Insertion Operation | Add Node | Adjacency Matrix | Data Structure
 
 # It will work for every type of graph like undirected unweighted etc
 
@@ -18,7 +18,7 @@ def add_node(v):
 
 # Python Program To Implement Graph Insertion Operation | Add Edge | Adjacency Matrix | Data Structure
 
-# undirected unweighted graph
+# unweighted undirected graph
 
 def add_edge_1(v1, v2):
     if v1 not in nodes:
@@ -31,9 +31,21 @@ def add_edge_1(v1, v2):
         graph[index1][index2] = 1
         graph[index2][index1] = 1
         
-# undirected weighted graph
+# unweighted directed graph
 
-def add_edge_2(v1, v2, cost):
+def add_edge_2(v1, v2):
+    if v1 not in nodes:
+        print(v1, "Not present in the graph")
+    elif v2 not in nodes:
+        print(v2, "Not present in the graph")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = 1
+        
+# weighted undirected graph
+
+def add_edge_3(v1, v2, cost):
     if v1 not in nodes:
         print(v1, "Not present in the graph")
     elif v2 not in nodes:
@@ -44,19 +56,7 @@ def add_edge_2(v1, v2, cost):
         graph[index1][index2] = cost
         graph[index2][index1] = cost
         
-# directed unweighted graph
-
-def add_edge_3(v1, v2):
-    if v1 not in nodes:
-        print(v1, "Not present in the graph")
-    elif v2 not in nodes:
-        print(v2, "Not present in the graph")
-    else:
-        index1 = nodes.index(v1)
-        index2 = nodes.index(v2)
-        graph[index1][index2] = 1
-        
-# directed weighted graph
+# weighted directed graph
 
 def add_edge_4(v1, v2, cost):
     if v1 not in nodes:
@@ -84,6 +84,33 @@ def delete_node(v):
         for n in graph:
             n.pop(index)
 
+# Python Program For Graph Deletion Operation | Delete Edge | Adjacency Matrix | Data Structure
+
+# unweighted undirected graph, weighted undirected graph
+
+def delete_edge_1(v1, v2):
+    if v1 not in nodes:
+        print(v1, "is not present in the graph")
+    elif v2 not in nodes:
+        print(v2, "is not present in the graph")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = 0
+        graph[index2][index1] = 0
+        
+# unweighted directed graph, weighted directed graph
+
+def delete_edge_2(v1, v2):
+    if v1 not in nodes:
+        print(v1, "is not present in the graph")
+    elif v2 not in nodes:
+        print(v2, "is not present in the graph")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = 0
+        
 def print_graph():
     for i in range(node_count):
         for j in range(node_count):
@@ -97,18 +124,8 @@ node_count = 0
 add_node("A")
 add_node("B")
 add_node("C")
-print(nodes)
-print(graph)
 
-add_edge_1("A", "B")
-add_edge_1("A", "C")
-
-print_graph()
-
-print()
-
-delete_node("A")
-print_graph()
+print_graph()"""
 
 
 
@@ -117,7 +134,9 @@ print_graph()
 
 
 
-"""# Python Program To Implement Graph Insertion Operation | Add Node | Adjacency List | Data Structure
+
+
+# Python Program To Implement Graph Insertion Operation | Add Node | Adjacency List | Data Structure
 
 def add_node(v):
     if v in graph:
@@ -135,29 +154,32 @@ def add_edge_1(v1, v2):
     elif v2 not in graph:
         print(v2, "is not present in the graph")
     else:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
+        if v2 not in graph[v1]:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+            
+# unweighted directed graph
+
+def add_edge_2(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the graph")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph")
+    else:
+        if v2 not in graph[v1]:
+            graph[v1].append(v2)
 
 # weighted undirected graph
 
-def add_edge_2(v1, v2, cost):
+def add_edge_3(v1, v2, cost):
     if v1 not in graph:
         print(v1, "is not present in the graph")
     elif v2 not in graph:
         print(v2, "is not present in the graph")
     else:
-        graph[v1].append((v2, cost))
-        graph[v2].append((v1, cost))
-
-# unweighted directed graph
-
-def add_edge_3(v1, v2):
-    if v1 not in graph:
-        print(v1, "is not present in the graph")
-    elif v2 not in graph:
-        print(v2, "is not present in the graph")
-    else:
-        graph[v1].append(v2)
+        if [v2, cost] not in graph[v1]:
+            graph[v1].append([v2, cost])
+            graph[v2].append([v1, cost])
 
 # weighted directed graph
 
@@ -167,21 +189,134 @@ def add_edge_4(v1, v2, cost):
     elif v2 not in graph:
         print(v2, "is not present in the graph")
     else:
-        graph[v1].append((v2, cost))
+        if [v2, cost] not in graph[v1]:
+            graph[v1].append([v2, cost])
+
+# Python Program For Graph Deletion Operation | Delete Node | Adjacency List | Data Structure
+
+# directed unweighted graph, undirected unweighted
+
+def delete_node_1(v):
+    if v not in graph:
+        print(v, "is not present in the graph")
+    else:
+        graph.pop(v)
+        for i in graph:
+            list1 = graph[i]
+            if v in list1:
+                list1.remove(v)
+
+# undirected weighted graph, directed weighted
+
+def delete_node_2(v):
+    if v not in graph:
+        print(v, "is not present in the graph")
+    else:
+        graph.pop(v)
+        for i in graph:
+            list1 = graph[i]
+            for edge in list1[:]:
+                if v == edge[0]:
+                    list1.remove(edge)
+
+# Python Program For Graph Deletion Operation | Delete Edge | Adjacency List | Data Structure
+
+# unweighted undirected graph
+
+def delete_edge_1(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the graph")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph")
+    else:
+        if v2 in graph[v1]:
+            graph[v1].remove(v2)
+            graph[v2].remove(v1)
+            
+# unweighted directed graph
+
+def delete_edge_2(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the graph")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph")
+    else:
+        if v2 in graph[v1]:
+            graph[v1].remove(v2)
+            
+# weighted undirected graph
+
+def delete_edge_3(v1, v2, cost):
+    if v1 not in graph:
+        print(v1, "is not present in the graph")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph")
+    else:
+        temp1 = [v1, cost]
+        temp2 = [v2, cost]
+        if temp2 in graph[v1]:
+            graph[v1].remove(temp2)
+            graph[v2].remove(temp1)
+            
+# weighted directed graph
+
+def delete_edge_4(v1, v2, cost):
+    if v1 not in graph:
+        print(v1, "is not present in the graph")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph")
+    else:
+        temp = [v2, cost]
+        if temp in graph[v1]:
+            graph[v1].remove(temp)
+
+# Python Program To Implement DFS Using Recursion | Data Structure
+
+# unweighted undirected graph
+
+def DFS_1(node, visited, graph):
+    if node not in graph:
+        print("Node is not present in the graph")
+        return
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for i in graph[node]:
+            DFS_1(i, visited, graph)
+            
+# weighted graph
+
+def DFS_2(node, visited, graph):
+    if node not in graph:
+        print("Node is not present in the graph")
+        return
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for i in graph[node]:
+            DFS_2(i[0], visited, graph)
+
+def print_graph():
+    for n in graph:
+        print(n, "-->" , graph[n])
 
 
 graph = {}
+visited = set()
 add_node("A")
 add_node("B")
 add_node("C")
 add_node("D")
-add_edge_4("A", "B", 10)
-add_edge_4("A", "C", 5)
-add_edge_4("C", "D", 20)
-print(graph)"""
+add_node("E")
 
-
-
+add_edge_1("A", "B")
+add_edge_1("B", "E")
+add_edge_1("A", "C")
+add_edge_1("A", "D")
+add_edge_1("B", "D")
+add_edge_1("C", "D")
+add_edge_1("E", "D")
+print_graph()
 
 
 
